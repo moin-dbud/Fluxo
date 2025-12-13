0ae06e2d430c7aede03c6b87378e26823b3df2c3
Moin sameer sheikh <132683286+moin-dbud@users.noreply.github.com>
2025-12-13 23:20:31 +0530

    First Commit

--- frontend/src/sections/ContactSection.jsx
+++ frontend/src/sections/ContactSection.jsx
@@ -16,7 +16,7 @@ const ContactSection = () => {
         };
 
         try {
-            const res = await fetch("http://localhost:5000/api/contact", {
+            const res = await fetch("https://fluxo-backend.onrender.com/contact", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
