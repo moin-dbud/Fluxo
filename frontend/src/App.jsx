7dbad48634543b644f7e94ecf8d7f5469fb17fea
Moin sameer sheikh <132683286+moin-dbud@users.noreply.github.com>
2025-12-15 21:58:04 +0530

    Implement routing with BrowserRouter and Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Onboarding from "./pages/Onboarding";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
